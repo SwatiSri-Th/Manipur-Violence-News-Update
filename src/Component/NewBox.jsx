79baf04cc65938c11a4a5f97e5ad50d5deb6ec23
Swati Sri Thingbaijam <thingbaijamswatisri@gmail.com>
2024-08-07 20:46:12 +0530

    newbox aos yadre update

--- src/Component/NewBox.jsx
+++ src/Component/NewBox.jsx
@@ -175,10 +175,34 @@ const NewBox = () => {
         </div>
 
         {/* NewBox Cards */}
-        <div>
+        <div data-aos="zoom-in">
           <Slider {...settings}>
             {NewBoxData.map((data) => (
-              <div>'hello'</div>
+              <div className="my-6">
+                <div
+                  key={data.id}
+                  className="flex flex-col gap-4 shadow-lg py-8 px-6 mx-4 rounded-xl dark:bg-gray-800 bg-yellow-200/10 relative"
+                >
+                  <div className="mb-4">
+                    <img
+                      src={data.img}
+                      alt=""
+                      className="rounded-full w-20 h-20"
+                    />
+                  </div>
+                  <div className=" flex flex-col items-center gap-4">
+                    <div className="space-y-3">
+                      <p className="text-xs text-gray-500">{data.text}</p>
+                      <h1 className="text-xl font-bold text-black/80 dark: text-white">
+                        {data.name}
+                      </h1>
+                    </div>
+                  </div>
+                  <p className="text-black/20 text-9xl font-serif absolute top-0 right-0">
+                    ''
+                  </p>
+                </div>
+              </div>
             ))}
           </Slider>
         </div>
